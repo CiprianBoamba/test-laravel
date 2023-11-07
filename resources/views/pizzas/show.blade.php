d502fb8dcc19a267f8f08d0d1cc5645284cf4b35
CiprianBoamba <ciprian.boamba@gmail.com>
2023-11-07 17:10:51 +0000

    add single show pizza details

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -5,12 +5,15 @@
 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen  bg-center bg-gray-100  dark:bg-gray-900">
   <div class="mx-auto">
     <div class="flex justify-center mx-auto p-6 lg:p-8 ">
-      <h1 class="underline text-3xl text-violet-600">Pizzas List-{{$id}}</h1>
+      <h1 class="underline text-3xl text-violet-600">Pizzas Details</h1>
 
     </div>
-    <div class="flex justify-center  mx-auto p-6 lg:p-8">
-      <p>id : {{$id}}</p>  
+    <div class="flex flex-col justify-center  mx-auto p-6 lg:p-8">
+      <h5>Order for {{$pizza->name}}</h5>  
+      <p>Type - {{$pizza->type}}</p>
+      <p>Base - {{$pizza->base}}</p>
     </div>
+    <a href="/" class=" text-blue-600">Back to all pizzas</a>
   </div>
 </div>
 
